87a7ff70252a1901f38385a2de9e6056eb61c5a0
Eric Oh <ericoh17@gmail.com>
2020-08-12 14:16:39 -0400

    model for known and unknown spec, sens

--- src/R/prevalence_simulation.R
+++ src/R/prevalence_simulation.R
@@ -3,6 +3,7 @@ library(cmdstanr)
 library(rstan)
 library(dplyr)
 library(tidyr)
+#library(posterior)
 
 # function for calculating the probability of a
 # sample testing positive
@@ -15,14 +16,14 @@ sample_positive <- function(prev, sens, spec) {
 
 # vector of true prevalences to consider
 #true_prevalence <- c(seq(0.001, 0.01, 0.001), seq(0.012, 0.05, 0.002))
-true_prevalence <- 0.1
+true_prevalence <- 0.005
 
 true_sens <- 0.95
 true_spec <- 0.995
 
 # number of samples to test
 # can be changed as desired
-n_samp <- 200
+n_samp <- 2000
 
 # give each sampled subject a covid status
 meas_status <- rbinom(n = n_samp,
@@ -31,18 +32,42 @@ meas_status <- rbinom(n = n_samp,
                                               true_sens,
                                               true_spec))
 
-prev_model <- cmdstan_model("src/stan/prevalence.stan")
+# fit model assuming known 
+# sensitivity and specificity
+prev_model_known <- cmdstan_model("src/stan/prevalence_known_sens_spec.stan")
 
-prev_model_data <- list(status = sum(meas_status), 
-                        n_samp = n_samp,
-                        logit_spec_prior_scale = 0.5,
-                        logit_sens_prior_scale = 0.5)
+prev_model_known_data <- list(status = sum(meas_status),
+                              n_samp = n_samp,
+                              sens = true_sens,
+                              spec = true_spec,
+                              logit_prev_prior_scale = 0.5)
 
-prev_fit <- prev_model$sample(data = prev_model_data, 
-                              refresh = 0, 
-                              parallel_chains = 4, 
-                              iter_warmup = 1e4, 
-                              iter_sampling = 1e4)
+prev_fit_known <- prev_model_known$sample(data = prev_model_known_data, 
+                                          refresh = 0, 
+                                          parallel_chains = 4, 
+                                          iter_warmup = 1e4, 
+                                          iter_sampling = 1e4)
 
-rstan::read_stan_csv(prev_fit$output_files())
+prev_known_results <- prev_fit_known$summary() %>% as.data.frame()
+
+
+# fit model assuming unknown
+# sensitivity and specificity
+prev_model_unknown <- cmdstan_model("src/stan/prevalence_unknown_sens_spec.stan")
+
+prev_model_unknown_data <- list(status = sum(meas_status),
+                                n_samp = n_samp,
+                                sens = true_sens,
+                                spec = true_spec,
+                                logit_spec_prior_scale = 0.5,
+                                logit_sens_prior_scale = 0.5,
+                                logit_prev_prior_scale = 0.5)
+
+prev_fit_unknown <- prev_model_unknown$sample(data = prev_model_unknown_data, 
+                                              refresh = 0, 
+                                              parallel_chains = 4, 
+                                              iter_warmup = 1e4, 
+                                              iter_sampling = 1e4)
+
+prev_unknown_results <- prev_fit_unknown$summary() %>% as.data.frame()
 
--- src/stan/prevalence_unknown_sens_spec.stan
+++ src/stan/prevalence_unknown_sens_spec.stan
@@ -3,29 +3,42 @@ data {
   int<lower = 0> n_samp;
   real<lower = 0> logit_spec_prior_scale;
   real<lower = 0> logit_sens_prior_scale;
+  real<lower = 0> logit_prev_prior_scale;
 }
 parameters {
-  real<lower = 0, upper = 1> prev;
+  // specificity
   real mu_logit_spec;
-  real mu_logit_sens;
   real<lower = 0> sigma_logit_spec;
-  real<lower = 0> sigma_logit_sens;
   real<offset = mu_logit_spec, multiplier = sigma_logit_spec> logit_spec;
+  // sensitivity
+  real mu_logit_sens;
+  real<lower = 0> sigma_logit_sens;
   real<offset = mu_logit_sens, multiplier = sigma_logit_sens> logit_sens;
+  // prevalence
+  real mu_logit_prev;
+  real<lower = 0> sigma_logit_prev;
+  real<offset = mu_logit_prev, multiplier = sigma_logit_prev> logit_prev;
 }
 transformed parameters {
   real spec = inv_logit(logit_spec);
   real sens = inv_logit(logit_sens);
+  real prev = inv_logit(logit_prev);
 }
 model {
   //likelihood
   real sample_positive = prev * sens + (1 - prev) * (1 - spec);
   status ~ binomial(n_samp, sample_positive);
   // priors
+  // specificity
   logit_spec ~ normal(mu_logit_spec, sigma_logit_spec);
-  logit_sens ~ normal(mu_logit_sens, sigma_logit_sens);
   sigma_logit_spec ~ normal(0, logit_spec_prior_scale);
+  mu_logit_spec ~ normal(4, 2);
+  // sensitivity
+  logit_sens ~ normal(mu_logit_sens, sigma_logit_sens);
   sigma_logit_sens ~ normal(0, logit_sens_prior_scale);
-  mu_logit_spec ~ normal(4, 2); 
   mu_logit_sens ~ normal(4, 2); 
+  // prevalence
+  logit_prev ~ normal(mu_logit_prev, sigma_logit_prev);
+  sigma_logit_prev ~ normal(0, logit_prev_prior_scale);
+  mu_logit_prev ~ normal(-5.5, 2); 
 }
